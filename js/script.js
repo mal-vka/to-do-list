41230256cd7ca1212337ff04056176d9ef400248
Malwina Gieraltowska <malwina.g@gmail.com>
2020-07-26 22:19:00 +0200

    binding events moved to a separate function

--- js/script.js
+++ js/script.js
@@ -14,7 +14,6 @@
         tasks.push({
             content: newTaskContent,            
         });
-
         render();
     };
 
@@ -28,21 +27,7 @@
         render();
     };
 
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString += `
-                <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
-                    <button class="js-done">czy zrobione?</button>
-                    <button class="js-remove">usuń</button>
-                    ${task.content}
-                </li>
-            `;
-        }
-
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-
+    const bindEvents = () => {
         const removeButtons = document.querySelectorAll(".js-remove");
         
         removeButtons.forEach((removeButton, index) => {
@@ -58,6 +43,24 @@
                 toggleTaskDone(index);
             });
         });
+    };
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+                <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
+                    <button class="js-done">czy zrobione?</button>
+                    <button class="js-remove">usuń</button>
+                    ${task.content}
+                </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;    
+        
+        bindEvents();
     };
 
     const onFormSubmit = (event) => {
